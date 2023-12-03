d5cbaa48763476ba168a9b8ab04333040b42d464
Firuzakerimova <bd7mbuwz1@code.edu.az>
2023-12-03 19:34:05 +0400

    JS Loop

---
+++ app.js
@@ -0,0 +1,45 @@
+// Task1
+// let a=prompt("eded daxil edin:");
+// for(let i=1;i<a;i++){
+//     if(i%2==1){
+//         console.log(i);
+//     }
+// }
+
+// Task2
+// let a=prompt('ededi daxil edin');
+// let b=0;
+// let c=0;
+// while(a>0){
+//     b=a%10
+//     c=c*10+b;
+//     a=parseInt(a/10);
+// }
+// console.log(c);
+
+// Task3
+// let  number=2;
+// let  times=5;
+// let a=number
+//  let c=0;
+// for(let i=0;i<times;i++){
+//     c+=a;
+//     a=a*10+number
+   
+// }
+// console.log(c);
+
+
+
+// Task4
+// let a=prompt('ededi daxil edin');
+// let b=0;
+// let c=1;
+// while(a>0){
+//     b=a%10 
+//     if(b%2==1){
+//         c=c*b 
+//     }
+//     a=parseInt(a/10);
+// }
+// console.log(c);
